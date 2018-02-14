af537ca4ed726a1fd39137a8e0a396c4e3327122
Georgiy Korneev <kgeorgiy@keorgiy.info>
2018-02-14 14:01:18 +0300

    GC removed

--- java/info/kgeorgiy/java/advanced/base/CG.java
+++
@@ -1,8 +0,0 @@
-package info.kgeorgiy.java.advanced.base;
-
-/**
- * @author Georgiy Korneev (kgeorgiy@kgeorgiy.info)
- */
-public interface CG {
-    public void certify(final Class<?> token, final String salt);
-}
