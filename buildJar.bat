5e07ccda32da93abffc9727f758598014c490906
Dogzik <levdovzhik@gmail.com>
2018-03-20 22:48:29 +0300

    Jar creation improved

--- buildJar.bat
+++ buildJar.bat
@@ -3,8 +3,12 @@ SET lib=lib\*
 SET test=artifacts\JarImplementorTest.jar
 SET dst=out\production\java-advanced-2018
 SET man=..\..\..\Manifest.txt
+SET dep=info\kgeorgiy\java\advanced\implementor\
 
 cd %proj%
 javac -d %dst% -cp %lib%;%test%; java\ru\ifmo\rain\dovzhik\implementor\Implementor.java
+
 cd %dst%
-jar cfm Implementor.jar %man% ru\ifmo\rain\dovzhik\implementor\*.class
+jar xf ..\..\..\%test% %dep%Impler.class %dep%JarImpler.class %dep%ImplerException.class
+jar cfm Implementor.jar %man% ru\ifmo\rain\dovzhik\implementor\*.class %dep%*.class
+rmdir info /s
